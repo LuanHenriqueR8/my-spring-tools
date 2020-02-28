6d2f008d379cb26b952e85590f3e28511cc1dcde
Luan Santos <luanhenriquer8@gmail.com>
2020-02-27 21:16:26 -0300

    add project

---
+++ src/main/java/br/com/luanhenriquer8/myspringtools/MySpringToolsApplication.java
@@ -0,0 +1,13 @@
+package br.com.luanhenriquer8.myspringtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luanhenriquer8/myspringtools/MySpringToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luanhenriquer8.myspringtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
