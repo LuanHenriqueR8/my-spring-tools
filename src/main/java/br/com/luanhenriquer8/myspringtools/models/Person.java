fc8c66e278b73c3449c26fe0aeadbc93f2056e4b
Luan Santos <luanhenriquer8@gmail.com>
2020-02-27 21:16:51 -0300

    add person model

---
+++ src/main/java/br/com/luanhenriquer8/myspringtools/models/Person.java
@@ -0,0 +1,16 @@
+package br.com.luanhenriquer8.myspringtools.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
